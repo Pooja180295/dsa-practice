21490ade299e388db2c1f633fa5a8cc24ad5868b
Pooja Sikchi <poojasikchi111@gmail.com>
2026-08-28 00:18:53 +0530

    Add ReverseStringBruteForce under Strings/ReverseString

---
+++ Programs/Strings/ReverseString/ReverseStringBruteForce.java
@@ -0,0 +1,20 @@
+//Reverse String - Brute Force
+class Main {
+    public static void main(String[] args) {
+        String str = "Pooja";
+        String reverse = "";
+        String reverseSb = "";
+        int n = str.length();
+        reverse = reverseString(str, n, reverse);
+        System.out.println(reverse);
+    }
+
+    public static String reverseString(String str, int n, String reverse){
+        for(int i=n-1;i>=0;i--){
+            char ch = str.charAt(i);
+            reverse = reverse + ch;
+        }
+        return reverse;
+    }
+
+}
