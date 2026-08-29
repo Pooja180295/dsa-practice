ca70b09c407df523c50822bf185f8759cff6ca10
Pooja Sikchi <poojasikchi111@gmail.com>
2026-08-30 00:05:47 +0530

    Add LinkedList programs: CreateLinkedList, FindLength, SearchAnElement

---
+++ Programs/LinkedList/CreateLinkedList/CreateLinkedList.java
@@ -0,0 +1,34 @@
+//Create Linked List
+import java.util.*;
+class Main {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+
+        a.next = b;
+        b.next = c;
+
+        Node current = a;
+        int count = 0; //find lenght of LL
+
+        while(current!=null){
+            System.out.println(current.data);
+            current = current.next;
+            count++;
+        }
+
+        System.out.println(count);
+
+   }
+}
